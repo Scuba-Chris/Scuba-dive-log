384d7b03cfb9181a3426297f9e8a9949b21a5024
Scuba-Chris <chrisceder@hotmail.com>
2019-11-05 17:38:55 -0800

    basic map on page

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+// 'use strict';
+
+// mapboxgl.accessToken = `${process.env.MAPBOX_API_KEY}`;
+// var map = new mapboxgl.Map({
+//   container: 'map',
+//   style: 'mapbox://styles/mapbox/streets-v11'
+// });
+// map.addControl(new mapboxgl.NavigationControl());
+
+
+// var layerList = document.getElementById('menu');
+// var inputs = layerList.getElementsByTagName('input');
+
+// function switchLayer(layer) {
+//   var layerId = layer.target.id;
+//   map.setStyle('mapbox://styles/mapbox/' + layerId);
+// }
+
+// for (var i = 0; i < inputs.length; i++) {
+//   inputs[i].onclick = switchLayer;
+// }
